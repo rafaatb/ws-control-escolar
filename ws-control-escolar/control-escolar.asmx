7481a0bfe3da4cfeadefb98fe92e1e665fc70294
Rafael Terrazas <Rafael Terrazas@INFORMATICA7>
2019-05-27 15:17:42 -0500

    2da entrega

---
+++ ws-control-escolar/control-escolar.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="control-escolar.asmx.cs" Class="ws_control_escolar.control_escolar" %>
---
+++ ws-control-escolar/control-escolar.asmx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace ws_control_escolar
+{
+    /// <summary>
+    /// Descripción breve de control_escolar
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class control_escolar : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+        [WebMethod]
+        public bool alumnoGuardar()
+        {
+            try
+            {
+                return true;
+            }catch(Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
